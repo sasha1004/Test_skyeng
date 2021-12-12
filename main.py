f216beea609c6c465a5bc8f7589dc645e527bddc
Александр К <sasha10041998@mail.ru>
2021-12-12 14:51:12 +0300

    test

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ if __name__ == '__main__':
     app = load_static(app)
     uvicorn.run(app=app,
                 host=os.getenv("HOST", "0.0.0.0"),
-                port=int(os.getenv("PORT", 8080)))
+                port=int(os.getenv("PORT", 5000)))
